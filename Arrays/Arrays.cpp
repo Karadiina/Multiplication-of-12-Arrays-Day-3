86bf48aab238165b1a34035a6ca69da713275e76
Kara Määttä <Karadiina.ma@gmail.com>
2020-12-06 15:09:25 +0200

    Implement second loop to create the array

--- Arrays/Arrays.cpp
+++ Arrays/Arrays.cpp
@@ -6,12 +6,16 @@ int main()
 	int base;
 	base = 12;
 	int i;
-	int number_array[12] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+	int number_array[13];
 
 	std::cout << "Multiplication of 12" << std::endl;
 	std::cout << "====================" << std::endl;
 
-	for (int i = 0; i < 12; i++) {
+
+	for (i = 0; i < 13; i++)
+		number_array[i] = i;
+
+	for (int i = 0; i < 13; i++) {
 
 		std::cout << base << " times " << number_array[i] << " is " << base * number_array[i] << std::endl;
 
